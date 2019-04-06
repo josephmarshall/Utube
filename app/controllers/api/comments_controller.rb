e9fb5eff759ec8837bcbc95e1e259b24f3cbb050
Brandon Parker <newboots@gmail.com>
2019-04-06 10:46:57 -0600

    comments model and controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,42 @@
+class Api::CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  def index
+    render json: Comment.all
+  end
+
+  def show
+    render json: @comment
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment
+    else
+      render json: comment.errors, status: 422
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: 422
+    end
+  end
+
+  def destroy
+    @comment.destroy
+  end
+
+  private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def product_params
+    params.require(:comment).permit(:body, :date, :user_id)
+  end
+
+end
