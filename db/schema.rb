c55d3af180963d535db3172bfda30cdea24049cc
Chris Larsen <c.r.larsen@hotmail.com>
2019-04-06 10:54:45 -0600

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_04_06_163113) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,4 +64,5 @@ ActiveRecord::Schema.define(version: 2019_04_06_163113) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
 end
