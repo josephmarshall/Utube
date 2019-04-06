97942d2b3a0509a8eb639b8870dcf8d2e24c2984
Luke Herrmann <herrmannluke@gmail.com>
2019-04-06 10:48:52 -0600

    Update Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,75 +1,83 @@
-import React from 'react'
-import { AuthConsumer, } from "../providers/AuthProvider";
-import { Segment, Menu, Button, Icon } from 'semantic-ui-react'
-import { Link, withRouter, } from 'react-router-dom'
+import React from "react";
+import { AuthConsumer } from "../providers/AuthProvider";
+import { Segment, Menu, Button, Icon } from "semantic-ui-react";
+import { Link, withRouter } from "react-router-dom";
 
 class Navbar extends React.Component {
-  
   rightNavItems = () => {
-    const { auth: { user, handleLogout, }, location, } = this.props;
-    
+    const {
+      auth: { user, handleLogout },
+      location
+    } = this.props;
+
     if (user) {
       return (
-        <Menu.Menu position='right'>
-            <Menu.Item>
-              <Button size="mini" color="red"> Upload Video</Button>
-            </Menu.Item>
-          <Menu.Item
-            onClick={ () => handleLogout(this.props.history) }
-          >
-          <Button icon size="mini" color="red"> 
-            <Icon name="sign-out" />
-          </Button>
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Button size="mini" color="red">
+              {" "}
+              Upload Video
+            </Button>
+          </Menu.Item>
+          <Menu.Item onClick={() => handleLogout(this.props.history)}>
+            <Button icon size="mini" color="red">
+              <Icon name="sign-out" />
+            </Button>
           </Menu.Item>
         </Menu.Menu>
-      )
+      );
     } else {
       return (
-        <Menu.Menu position='right'>
-          <Link to='/login'>
+        <Menu.Menu position="right">
+          <Link to="/login">
             <Menu.Item
-              id='login'
-              name='login'
-              active={location.pathname === '/login'}
+              id="login"
+              name="login"
+              active={location.pathname === "/login"}
             />
           </Link>
-          <Link to='/register'>
+          <Link to="/register">
             <Menu.Item
-              id='register'
-              name='register'
-              active={location.pathname === '/register'}
+              id="register"
+              name="register"
+              active={location.pathname === "/register"}
             />
           </Link>
         </Menu.Menu>
-      )
+      );
     }
-  }
-  
+  };
+
   render() {
     return (
       <div>
         <Menu pointing secondary>
-          <Link to='/'>
+          <Link to="/">
             <Menu.Item
-              Icon={<img width='50' height='25' src="https://imgur.com/a/MS0dxCo?" />}
-              active={this.props.location.pathname === '/'} />
+              Icon={
+                <img
+                  width="50"
+                  height="25"
+                  src="https://imgur.com/a/MS0dxCo?"
+                />
+              }
+              active={this.props.location.pathname === "/"}
+            />
           </Link>
-            { this.rightNavItems() }
+          {this.rightNavItems()}
         </Menu>
       </div>
-    )
+    );
   }
 }
 
 export class ConnectedNavbar extends React.Component {
   render() {
     return (
-      <AuthConsumer> 
-        { auth => 
-          <Navbar { ...this.props } auth={auth} />
-        }
+      <AuthConsumer>
+        {auth => <Navbar {...this.props} auth={auth} />}
       </AuthConsumer>
-    )
+    );
   }
 }
 
